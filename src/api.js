014560f59b3b941c1bbceaac4aa2cb8c1c40f8e1
viktoriiass <vikakotljarevskaja@gmail.com>
2025-06-16 15:10:44 +0200

    Добавил api.js и поправил axios-запросы

---
+++ src/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+
+const api = axios.create({
+  baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export function setToken(token) {
+  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
+export default api;
